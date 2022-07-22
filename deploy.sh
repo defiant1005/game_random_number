8353daf729d659a1cdd1881fff354c9e5603323e
rinat.idiiatov <rinat.idiiatov@qlever.com>
2022-07-22 13:42:00 +0300

    добавил настройки для деплоя

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/defiant1005/game_random_number.git master:gh-pages
+
+cd -
